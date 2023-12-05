eb777b60fcad098411fa5c89f1193c3e49234688
AiHughesToo <alex@aihughes.dev>
2023-12-05 10:21:31 -0500

    troubleshooting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:show, :update, :destroy, :create]
   before_action :authenticate_token!, except: [:password_reset, :index]
 
   # GET /users
@@ -16,12 +16,14 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    if @current_user.account_type == "admin"
+      @user = User.new(user_params)
   
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+      if @user.save
+        render json: @user, status: :created, location: @user
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     end
   end
 
