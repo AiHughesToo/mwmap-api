3115323855ab538f523140bcfd4ef000782b12e7
AiHughesToo <alex@aihughes.dev>
2023-12-06 10:49:32 -0500

    logic for location sorting and lead delivery

--- app/mailers/location_mailier.rb
+++ app/mailers/location_mailier.rb
@@ -1,7 +1,7 @@
 class LocationMailer < ApplicationMailer
 
 
-    def new_lead(locations, s_name, s_phone, s_email)
+    def new_lead_email(locations, s_name, s_phone, s_email)
     #   @user = user
     #   @url = ResponseHelper::PASSWORD_RESET_URL
     #   mail(to: @user.email, subjet: ResponseHelper::WELCOME_SUBJECT_LINE)
