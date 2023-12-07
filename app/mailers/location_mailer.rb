bebe48f732a0f4f2de1d5a23e431e56662879ec7
AiHughesToo <alex@aihughes.dev>
2023-12-07 13:18:31 -0500

    mailer for map leads

--- app/mailers/location_mailer.rb
+++ app/mailers/location_mailer.rb
@@ -1,26 +1,24 @@
 class LocationMailer < ApplicationMailer
 
-
     def lead_for_all_email(email, s_name, s_phone, s_email)
-      p "im in the locations mailer."
-      p email
-      p s_name
-      @s_name = s_name
-      @s_email = s_email
-      @s_phone = s_phone
-      mail(to: email, subjet: "New Map Lead")
-
-    end
-
-
-    def lead_for_one_email(email, s_name, s_phone, s_email)
-     p s_name
-      @s_name = s_name
-      @s_email = s_email
-      @s_phone = s_phone
-      mail(to: email, subjet: "New Exclusive Map Lead")
-
-    end
+        p "im in the locations mailer."
+        p email
+        p s_name
+        @s_name = s_name
+        @s_email = s_email
+        @s_phone = s_phone
+        mail(to: email, subjet: "New Map Lead")
   
-    
-  end
+      end
+  
+  
+      def lead_for_one_email(email, s_name, s_phone, s_email)
+       p s_name
+        @s_name = s_name
+        @s_email = s_email
+        @s_phone = s_phone
+        mail(to: email, subjet: "New Exclusive Map Lead")
+  
+      end
+      
+end
