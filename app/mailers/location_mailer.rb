4c7837fb92584274615c8079c918c8ee2677e1af
AiHughesToo <alex@aihughes.dev>
2023-12-07 12:13:48 -0500

    basic logic for lead delivery

--- app/mailers/location_mailer.rb
+++ app/mailers/location_mailer.rb
@@ -4,6 +4,7 @@ class LocationMailer < ApplicationMailer
     def lead_for_all_email(email, s_name, s_phone, s_email)
       p "im in the locations mailer."
       p email
+      p s_name
       @s_name = s_name
       @s_email = s_email
       @s_phone = s_phone
@@ -13,8 +14,7 @@ class LocationMailer < ApplicationMailer
 
 
     def lead_for_one_email(email, s_name, s_phone, s_email)
-      p "im in the locations mailer."
-      p email
+     p s_name
       @s_name = s_name
       @s_email = s_email
       @s_phone = s_phone
