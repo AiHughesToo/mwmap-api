0cc8134233ef50a0a03c64100c6ba1711a86efe9
AiHughesToo <alex@aihughes.dev>
2023-12-12 11:49:12 -0500

    update location service column type

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,3 +1,3 @@
 class LocationSerializer < ActiveModel::Serializer
-  attributes :id, :name, :latitude, :longitude, :wp_user_id, :location_type, :image, :web, :social_one, :social_two, :email, :phone, :calendly
+  attributes :id, :name, :latitude, :longitude, :wp_user_id, :location_type, :image, :web, :social_one, :social_two, :email, :phone, :calendly, :service_types
 end
