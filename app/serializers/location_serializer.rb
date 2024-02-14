22f01faefecfbe68f55fcef0ffb4f324a6adbcf4
AiHughesToo <alex@aihughes.dev>
2024-02-14 16:22:03 -0500

    setting up user map locations list

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,3 +1,4 @@
 class LocationSerializer < ActiveModel::Serializer
-  attributes :id, :name, :latitude, :longitude, :wp_user_id, :location_type, :image, :web, :social_one, :social_two, :email, :phone, :calendly, :service_types, :description, :address_l1, :address_l2, :address_state, :address_city, :address_zip
+  attributes :id, :name, :latitude, :longitude, :wp_user_id, :location_type, :image, :web, :social_one, :social_two, :email, :phone, :calendly, :service_types, 
+  :description, :address_l1, :address_l2, :address_state, :address_city, :address_zip, :location_active
 end
